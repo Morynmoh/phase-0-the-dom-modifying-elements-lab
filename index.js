3d8af08d3ff3ee9f68bba59f30b7efbcdd75cb24
Maureen <morynmoh@gmail.com>
2023-03-26 14:32:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainNode = document.querySelector('#main');
+mainNode.parentNode.removeChild(mainNode);
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Maureen is the champion';
+
+
+
+
+//h1.textContent = "YOUR-NAME is the champion";
